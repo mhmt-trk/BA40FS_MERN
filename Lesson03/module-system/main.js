063440573db78ccb308beef9e518cc26b3111114
mhmt-trk <121402089+mhmt-trk@users.noreply.github.com>
2023-03-26 11:28:27 +0300

    ilk

---
+++ Lesson03/module-system/main.js
@@ -0,0 +1,8 @@
+console.log("Hello from main.js");
+
+import { sumNum, divNum } from "./mathModule.js";
+
+console.log("SUM: ", sumNum(2, 3));
+console.log("DIV: ", divNum(2, 3));
+
+
---
+++ Lesson03/module-system/mathModule.js
@@ -0,0 +1,15 @@
+/*V1
+function sum(x,y){
+    return x+y;
+}
+*/
+
+/*V2
+const sum = function(x,y){
+    return x+y;
+}
+*/
+
+//V3
+export const sumNum = (x, y) => x + y;
+export const divNum = (x, y) => x / y;
